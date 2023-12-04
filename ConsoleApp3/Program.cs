b064cc5dc5b9004aec84de5b3595f8cbeb6d3a05
MinJae1026 <ksengboss123@naver.com>
2023-12-04 15:27:38 +0900

    2023/12/04 가상함수 원리

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Unit
+    {
+        public string name;
+        public int health;
+        public int Maxhealth;
+
+        public Unit(string name, int health, int Maxhealth)
+        {
+            this.name = name;
+            this.health = health;
+            this.Maxhealth = Maxhealth;
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"유닛:{name}");
+            Console.WriteLine($"체력:{health}");
+            Console.WriteLine($"최대체력:{Maxhealth}");
+        }
+    }
+
+    class Marine : Terran
+    {
+        public Marine()
+        {
+            health = 40;
+            maxHealth = health;
+        }
+
+        public override void Recovery()
+        {
+            health = maxHealth;
+            Console.WriteLine("health:" + health);
+        }
+    }
+    class Firebet : Terran
+    {
+        public Firebet()
+        {
+            health = 50;
+            maxHealth = health;
+        }
+
+        public override void Recovery()
+        {
+            health = maxHealth;
+            Console.WriteLine("health:" + health);
+        }
+    }
+
+    class Ghost : Terran
+    {
+        public Ghost()
+        {
+            health = 45;
+            maxHealth = health;
+        }
+
+        public override void Recovery()
+        {
+            health = maxHealth;
+            Console.WriteLine("health:" + health);
+        }
+    }
+
+   
+
+    internal class Program
+    {
+        static void Beacon(Terran terran)
+        {
+            terran.Recovery();
+        }
+
+        static void Main(string[] args)
+        {
+           Marine marine = new Marine();
+           
+           Firebet firebet = new Firebet();
+           Ghost ghost = new Ghost();
+
+            marine.Health -= 10;
+            firebet.Health -= 10;
+            ghost.Health -= 10;
+
+            Beacon(marine);
+            Beacon(firebet);
+            Beacon(ghost);
+
+        }
+    }
+}
---
+++ ConsoleApp3/Terran.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal abstract class Terran
+    {
+        protected int health;
+        protected int maxHealth;
+
+        public int Health
+        {
+            get { return health; }
+            set 
+            {
+                if(maxHealth < value)
+                {
+                    Console.WriteLine("");
+                    return;
+                }
+
+                health = value;
+                Console.WriteLine("health의 값: " + health);
+            }
+        }
+        
+        public abstract void Recovery();
+    }
+}
